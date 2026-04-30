1935d802412e051bf19a97904f59728bc3583d5a
POLYPROPICKS <tureckisun@gmail.com>
2026-04-30 18:06:30 +0300

    Expose PreMVP landing on production route

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-import StatusBar from '@/components/layout/StatusBar';
-import HeaderBar from '@/components/layout/HeaderBar';
-import MarketSourcesSection from '@/components/sections/MarketSourcesSection';
-import PremiumEventsSection from '@/components/sections/PremiumEventsSection';
-import CTASection from '@/components/sections/CTASection';
+import ReconstructionPage from './reconstruction/page';
 
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-black text-white">
-      <StatusBar />
-      <HeaderBar />
-      <MarketSourcesSection />
-      <PremiumEventsSection />
-      <CTASection />
-    </div>
-  );
+  return <ReconstructionPage />;
 }
